dcfa3b69f5ffd882b68d3e6aeaf54f284b0ed016
yutian-pc <yutiancoding@gmail.com>
2022-06-07 11:08:17 -0400

    file upload

--- MarketplaceWebPortal/Controllers/ProductController.cs
+++ MarketplaceWebPortal/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MarketplaceWebPortal.Controllers
 {
-    // [Authorize(Policy = "Authenticated")]
+    //[Authorize(Policy = "Authenticated")]
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -39,6 +39,43 @@ namespace MarketplaceWebPortal.Controllers
         {
             Product[] results = _context.Products.Where(u => u.Type == "Electrical").ToArray();
             return View(results);
+        }
+
+        public IActionResult UploadImage()
+        {
+            SingleFileModel model = new SingleFileModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Upload(SingleFileModel model)
+        {
+           
+            //if (ModelState.IsValid)
+            {
+               
+                model.IsResponse = true;
+
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+                
+                //create folder if not exist
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                //get file extension
+                FileInfo fileInfo = new FileInfo(model.File.FileName);
+                string fileName = model.FileName + fileInfo.Extension;
+                
+                string fileNameWithPath = Path.Combine(path, fileName);
+                
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    model.File.CopyTo(stream);
+                }
+                model.IsSuccess = true;
+                TempData["Success"] = "File upload successfully";
+            }
+            return View("UploadImage", model);
         }
     }
 }
